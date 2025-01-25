b71088ebabb5d05cf37910d5e9a265d4cf8cbe14
Oleksandr Prokopchuk <sasha195@i.ua>
2025-01-25 17:59:48 +0200

    Renamed component to components

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 // Імпорт стилів нормалізації
-import "modern-normalize";
-import "./index.css";
-import App from "./component/App.jsx";
+import 'modern-normalize';
+import './index.css';
+import App from './components/App.jsx';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
