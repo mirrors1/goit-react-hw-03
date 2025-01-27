bd54fd6984ffdf9dcb2410eb1d81181b38bc8258
Oleksandr Prokopchuk <sasha195@i.ua>
2025-01-27 15:08:37 +0200

    add tooltip

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,20 +1,31 @@
 import { FaUserLarge, FaPhone } from 'react-icons/fa6';
 import { IconContext } from 'react-icons';
 import s from './Contact.module.css';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
+import { nanoid } from 'nanoid';
 
 export default function Contact({ data: { id, name, number }, onDelete }) {
+  const idTolltip = nanoid();
   const iconDataValue = {
     className: 'global-class-name-icon',
     size: '18px',
   };
   return (
     <>
-      <div className={s.contact}>
+      <div data-tooltip-id={idTolltip} className={s.contact}>
         <div className={s.containerTitle}>
           <IconContext.Provider value={iconDataValue}>
             <FaUserLarge className={s.icon} />
           </IconContext.Provider>
           <p className={s.name}>{name}</p>
+          {name.length > 14 && (
+            <ReactTooltip
+              id={idTolltip}
+              place="bottom"
+              variant="info"
+              content={name}
+            />
+          )}
         </div>
         <div className={s.containerPhone}>
           <IconContext.Provider value={iconDataValue}>
